2e719b508f92e8df31ecbbc15ebe9c4906bf0707
joe <joe@jjssoftware.co.uk>
2017-01-21 09:41:20 +0000

    - ESP8266-ified with ICACHE_FLASH_ATTR - Stripped AVR specific code - Reformatted/prettified

--- DHT.h
+++ DHT.h
@@ -1,17 +1,16 @@
 /* DHT library
 
-MIT license
-written by Adafruit Industries
-*/
+ MIT license
+ written by Adafruit Industries
+
+ This is an ESP8266 flavour of the original Adafruit DHT library
+
+ */
 #ifndef DHT_H
 #define DHT_H
 
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
-
+#include "Arduino.h"
+#include "interrupts.h"
 
 // Uncomment to enable printing out nice debug messages.
 //#define DHT_DEBUG
@@ -21,11 +20,11 @@ written by Adafruit Industries
 
 // Setup debug printing macros.
 #ifdef DHT_DEBUG
-  #define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
-  #define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
+#define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
+#define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
 #else
-  #define DEBUG_PRINT(...) {}
-  #define DEBUG_PRINTLN(...) {}
+#define DEBUG_PRINT(...) {}
+#define DEBUG_PRINTLN(...) {}
 #endif
 
 // Define types of sensors.
@@ -34,42 +33,23 @@ written by Adafruit Industries
 #define DHT21 21
 #define AM2301 21
 
-
 class DHT {
-  public:
-   DHT(uint8_t pin, uint8_t type, uint8_t count=6);
-   void begin(void);
-   float readTemperature(bool S=false, bool force=false);
-   float convertCtoF(float);
-   float convertFtoC(float);
-   float computeHeatIndex(float temperature, float percentHumidity, bool isFahrenheit=true);
-   float readHumidity(bool force=false);
-   boolean read(bool force=false);
+public:
+	ICACHE_FLASH_ATTR DHT(uint8_t pin, uint8_t type, uint8_t count = 6);
+	void ICACHE_FLASH_ATTR begin(void);
+	float ICACHE_FLASH_ATTR readTemperature(bool S = false, bool force = false);
+	float ICACHE_FLASH_ATTR convertCtoF(float);
+	float ICACHE_FLASH_ATTR convertFtoC(float);
+	float ICACHE_FLASH_ATTR computeHeatIndex(float temperature, float percentHumidity, bool isFahrenheit = true);
+	float ICACHE_FLASH_ATTR readHumidity(bool force = false);
 
- private:
-  uint8_t data[5];
-  uint8_t _pin, _type;
-  #ifdef __AVR
-    // Use direct GPIO access on an 8-bit AVR so keep track of the port and bitmask
-    // for the digital pin connected to the DHT.  Other platforms will use digitalRead.
-    uint8_t _bit, _port;
-  #endif
-  uint32_t _lastreadtime, _maxcycles;
-  bool _lastresult;
-
-  uint32_t expectPulse(bool level);
-
-};
-
-class InterruptLock {
-  public:
-   InterruptLock() {
-    noInterrupts();
-   }
-   ~InterruptLock() {
-    interrupts();
-   }
-
+private:
+	uint8_t data[5];
+	uint8_t _pin, _type;
+	uint32_t _lastreadtime, _maxcycles;
+	bool _lastresult;
+	bool read(bool force = false);
+	uint32_t expectPulse(bool level);
 };
 
 #endif
